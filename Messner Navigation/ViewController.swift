18e66712f6363596598a9ec1fb6c76b2bcd38f4e
Brad Messner <bradmessner@brads-macbook-pro.local>
2019-04-02 06:48:07 -0400

    Initial Commit

---
+++ Messner Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messner Navigation
+//
+//  Created by Brad D. Messner on 4/2/19.
+//  Copyright © 2019 Brad D. Messner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

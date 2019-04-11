4a884f3e96d41b0495bd32382f417f2f0de7ec43
Brad D Messner <bmessner@setonhill.edu>
2019-04-11 05:36:03 -0400

    Version 1

--- Messner Navigation/ViewController.swift
+++ Messner Navigation/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
 
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    let kshuLatitude: CLLocationDegrees = 40.3114
+    let kshuLongitude: CLLocationDegrees = -79.5567
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation: CLLocation=locations[0]
+        NSLog("Something is happening")
+        
+        // horizontal accuracy less than than 0 means failure at gps level
+        if newLocation.horizontalAccuracy >= 0 {
+            let shu:CLLocation = CLLocation(latitude: kshuLatitude,longitude: kshuLongitude)
+            let delta:CLLocationDistance = shu.distance(from: newLocation)
+            let miles: Double = (delta * 0.000621371) + 0.5 // meters to rounded miles
+            if miles < 3 {
+                // Stop updating the location
+                locMan.stopUpdatingLocation()
+                // Congratulate the user
+                distanceLabel.text = "Enjoy\nSeton Hill University!"
+            } else {
+                let commaDelimited: NumberFormatter = NumberFormatter()
+                commaDelimited.numberStyle = NumberFormatter.Style.decimal
+                distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to SHU"
+            }
+            //waitView.isHidden = true
+            //distanceView.isHidden = false
+        }
+        // oldLocation=locations[0] as CLLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        //waitView.isHidden = true
+        //distanceView.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locMan.delegate = self
+        locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locMan.distanceFilter = 1609; // a mile
+        locMan.requestWhenInUseAuthorization()
+        locMan.startUpdatingLocation()
+        startLocation = nil
     }
 
 
